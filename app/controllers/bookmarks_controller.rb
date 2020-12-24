5ec6ff7f01017088cc1e8a7b6ce434808abccd21
eulis01 <eulis01@gmail.com>
2020-12-24 00:17:55 -0500

    add home method and removed generated code

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,15 @@
 class BookmarksController < ApplicationController
   before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
 
+  def home
+  end
+
   # GET /bookmarks
-  # GET /bookmarks.json
   def index
     @bookmarks = Bookmark.all
   end
 
   # GET /bookmarks/1
-  # GET /bookmarks/1.json
   def show
   end
 
@@ -22,49 +23,32 @@ class BookmarksController < ApplicationController
   end
 
   # POST /bookmarks
-  # POST /bookmarks.json
   def create
     @bookmark = Bookmark.new(bookmark_params)
-
-    respond_to do |format|
       if @bookmark.save
-        format.html { redirect_to @bookmark, notice: 'Bookmark was successfully created.' }
-        format.json { render :show, status: :created, location: @bookmark }
+        redirect_to @bookmark, notice: 'Bookmark was successfully created.' 
       else
-        format.html { render :new }
-        format.json { render json: @bookmark.errors, status: :unprocessable_entity }
+        render 'form'
       end
     end
   end
 
   # PATCH/PUT /bookmarks/1
-  # PATCH/PUT /bookmarks/1.json
+
   def update
-    respond_to do |format|
-      if @bookmark.update(bookmark_params)
-        format.html { redirect_to @bookmark, notice: 'Bookmark was successfully updated.' }
-        format.json { render :show, status: :ok, location: @bookmark }
-      else
-        format.html { render :edit }
-        format.json { render json: @bookmark.errors, status: :unprocessable_entity }
-      end
-    end
+
   end
 
   # DELETE /bookmarks/1
-  # DELETE /bookmarks/1.json
+
   def destroy
-    @bookmark.destroy
-    respond_to do |format|
-      format.html { redirect_to bookmarks_url, notice: 'Bookmark was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_bookmark
-      @bookmark = Bookmark.find(params[:id])
+      @bookmark = Bookmark.find_by(id: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
