f610437fd3974be113db10d82d07f45036452d3f
eulis01 <eulis01@gmail.com>
2020-12-24 00:15:23 -0500

    removed code generated with scaffold

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,13 +2,11 @@ class TagsController < ApplicationController
   before_action :set_tag, only: [:show, :edit, :update, :destroy]
 
   # GET /tags
-  # GET /tags.json
   def index
     @tags = Tag.all
   end
 
   # GET /tags/1
-  # GET /tags/1.json
   def show
   end
 
@@ -22,42 +20,24 @@ class TagsController < ApplicationController
   end
 
   # POST /tags
-  # POST /tags.json
   def create
     @tag = Tag.new(tag_params)
-
-    respond_to do |format|
       if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render :show, status: :created, location: @tag }
+        redirect_to @tag, notice: 'Tag was successfully created.' 
       else
-        format.html { render :new }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
+        render 'form'
       end
-    end
   end
 
   # PATCH/PUT /tags/1
-  # PATCH/PUT /tags/1.json
   def update
-    respond_to do |format|
-      if @tag.update(tag_params)
-        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tag }
-      else
-        format.html { render :edit }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
-    end
+
   end
 
   # DELETE /tags/1
-  # DELETE /tags/1.json
   def destroy
-    @tag.destroy
-    respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
-      format.json { head :no_content }
+    @tag.destroy :id
+    redirect_to tags_url, notice: 'Tag was successfully destroyed.' 
     end
   end
 
