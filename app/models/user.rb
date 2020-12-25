3dc81a7e93a95af638ac912e25781ca0430426f7
eulis01 <eulis01@gmail.com>
2020-12-25 15:52:15 -0500

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :tags
   has_many :bookmarks, through: :tags
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
