51424a50b7aa0d1a7021fe6ce5483fe48b482783
eulis01 <eulis01@gmail.com>
2020-12-25 15:31:46 -0500

    testing facebook auth login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.allowed_request_methods = [:post, :get]
     #provider :developer unless Rails.env.production?
     #provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-    #provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
     #provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRECT']
 
 end
