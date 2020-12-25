9a05a26249799a27c9e8ebf12b1225319344ad3b
eulis01 <eulis01@gmail.com>
2020-12-25 15:52:05 -0500

    added validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
   belongs_to :bookmark
   belongs_to :user
+  validate :name, presece: true, uniqueness: true
+  validate :color, presece: true, uniqueness: true
+  
 end
