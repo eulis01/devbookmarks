dfda0d566a0ccd49160145b948520836d25b468d
eulis01 <eulis01@gmail.com>
2020-12-25 15:35:14 -0500

    testing routes commented out

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'bookmarks#home'
-  
-  get '/auth/github/callback', to: 'sessions#create'
-  get '/auth/facebook/callback', to: 'sessions#create'
-  get '/auth/twitter/callback', to: 'sessions#create'
+  root to: 'bookmarks#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/singup', to: 'users#new'
+  post '/singup', to: 'users#create'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  #get '/auth/github/callback', to: 'sessions#create'
+  #get '/auth/facebook/callback', to: 'sessions#create'
+  #get '/auth/twitter/callback', to: 'sessions#create'
 
 
-  resources :sessions
+  #resources :sessions
   resources :tags
   resources :bookmarks
-  resources :users
+  #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
