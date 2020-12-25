54090b8f2eeac50f10f566217329032f7a99d0dd
eulis01 <eulis01@gmail.com>
2020-12-25 15:54:51 -0500

    fix typos

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   has_many :tags
   has_many :user, through: :tags
-  validate :name, presece: true, uniqueness: true
-  validate :url, presece: true, uniqueness: true
+  validates :name, presece: true, uniqueness: true
+  validates :url, presece: true, uniqueness: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   belongs_to :bookmark
   belongs_to :user
-  validate :name, presece: true, uniqueness: true
-  validate :color, presece: true, uniqueness: true
+  validates :name, presece: true, uniqueness: true
+  validates :color, presece: true, uniqueness: true
   
 end
