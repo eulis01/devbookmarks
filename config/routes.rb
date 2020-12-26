32fd5c5325abcf5c6d2de4c277fa7ad3d561bae1
eulis01 <eulis01@gmail.com>
2020-12-26 14:18:25 -0500

    moved around location

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   root to: 'bookmarks#index'
+  match '/auth/:provider/callback', to: 'sessions#facebook_login', via: [:get, :post]
+  get '/auth/facebook/callback', to: 'sessions#facebook_login'
+  #get '/auth/github/callback', to: 'sessions#create'
+  #get '/auth/twitter/callback', to: 'sessions#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/singup', to: 'users#new'
-  post '/singup', to: 'users#create'
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  #get '/auth/github/callback', to: 'sessions#create'
-  #get '/auth/facebook/callback', to: 'sessions#create'
-  #get '/auth/twitter/callback', to: 'sessions#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
-
-  #resources :sessions
-  resources :tags
   resources :bookmarks
+  resources :tags
+  #resources :sessions
   #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
