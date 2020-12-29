895c49f02cf752f3c22165ec7237b43b4cd50d91
eulis01 <eulis01@gmail.com>
2020-12-28 23:55:08 -0500

    added proper session create and omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,34 @@
 class SessionsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:new, :create]
+  skip_before_action :redirect_if_not_logged_in
 
-  def new	
-    @user = User.new	
-  end	
+  def new
+    @user = User.new
+  end
 
   def create	
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth['info']['name']
-      u.email = auth['info']['email']
+      @user = User.find_by(email: params[:user][:email])
+      if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Wrong login. Please try again."
+        render :new
     end
-    session[:id] = @user.id
-    render 'bookmarks/home'
   end	
 
-  def destroy	
-    session.delete :id
-    redirect_to root_path	
+  def omniauth
+    @user = User.from_omniauth(auth)
+    if @user.valid?
+    session[:user_id] = @user.id
+    redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path	
   end
 
   protected
