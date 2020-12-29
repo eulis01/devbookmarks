4944c2c5a79ceff2a0732e24a3cd8d4225a49e28
eulis01 <eulis01@gmail.com>
2020-12-29 00:00:09 -0500

    proper omniauth route and destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   root to: 'bookmarks#index'
   match '/auth/:provider/callback', to: 'sessions#facebook_login', via: [:get, :post]
-  get '/auth/facebook/callback', to: 'sessions#facebook_login'
+  get '/auth/facebook/callback', to: 'sessions#omniauth'
   #get '/auth/github/callback', to: 'sessions#create'
   #get '/auth/twitter/callback', to: 'sessions#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
   resources :bookmarks
   resources :tags
-  #resources :sessions
-  #resources :users
+  resources :sessions
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
