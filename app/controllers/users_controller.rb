95bea19555fb144fc1d171feb20aa2e34399d0b9
eulis01 <eulis01@gmail.com>
2020-12-28 23:55:46 -0500

    add propper new user logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,70 @@
 class UsersController < ApplicationController
-  skip_before_action :verified_user, only: [:new, :create]
+  skip_before_action :redirect_if_not_logged_in, only: [:new, :create]
+  before_action :set_user, only: [:edit]
 
-
-  # GET /users
   def index
     @users = User.all
   end
 
   # GET /users/1
   def show
-    current_user
+    set_user
+    @user = User.find_by(id: params[:id])
   end
 
   # GET /users/new
   def new
     @user = User.new
   end
-
-  # GET /users/1/edit
-  def edit
-  end
-
+  
   # POST /users
   def create
-    @user = User.create(user_params)
-    return redirect_to controller: 'users', action: 'new' unless @user.save
-    session[:id] = @user.id
-    redirect_to controller: 'bookmarks', action: 'index'
+    @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        lash[:message] = "Your account was created"
+        redirect_to root_path(@user)
+      else
+        render :new
+      end
+  end
+  
+  # GET /users/1/edit
+  def edit
+    set_user
+    #@user = User.find_by_id(params[:id])
+    if @user && logged_in?
+        render :edit
+    else
+        flash[:error] = "You don't have permission"
+    end  
   end
 
   # PATCH/PUT /users/1
   def update
-
+    set_user
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   # DELETE /users/1
   def destroy
-    @user = current_user
-    if @user.destroy
-      redirect_to users_url, notice: 'User was successfully destroyed.'
-    else 
-      render 'form'
-    end
+    set_user
+    @user.delete
+    flash[:error] = "You have successfully deleted your account!"
+    redirect_to signup_path 
   end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :description, :password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find_by_id(params[:id])
     end
 end
