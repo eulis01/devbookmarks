9a0eaf2f780b2e8617ab1337bb094e92fb965cec
eulis01 <eulis01@gmail.com>
2020-12-28 23:56:35 -0500

    add create user from omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   has_many :bookmarks, through: :tags
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def self.from_omniauth(auth)
+    find_or_create_by(username: auth[:info][:email]) do |user|
+      user.password = SecureRandom.hex(15)
+      end
+  end
 end
